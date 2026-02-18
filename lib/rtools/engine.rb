e7e97f83bade36b2b0f5aab2e372a666497a2db4
Barbarisych <zxcdevelopment@gmail.com>
2026-02-18 04:08:46 +0200

    refactor: Remove unnecessary autoload_paths initializer
    The proper directory structure (app/controllers/rtools/) makes the
    manual autoload_paths configuration unnecessary with isolate_namespace.

--- lib/rtools/engine.rb
+++ lib/rtools/engine.rb
@@ -4,15 +4,6 @@ module Rtools
   class Engine < ::Rails::Engine
     isolate_namespace Rtools
 
-    # Add app/ directories to autoload paths once the engine is initialized
-    initializer "rtools.autoload_paths", before: :eager_load do
-      config.autoload_paths += %W[
-        #{root}/app/controllers
-        #{root}/app/services
-        #{root}/app/helpers
-      ]
-    end
-
     routes do
       resources :performance_profiles, only: %i[index show], format: false
     end
