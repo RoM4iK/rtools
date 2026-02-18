96928a027baba6aa14da3dd68ca819255dac2cc7
Barbarisych <zxcdevelopment@gmail.com>
2026-02-18 04:06:40 +0200

    fix: Explicitly add app/ directories to engine autoload paths
    Ensure controllers, services, and helpers in app/ are autoloaded by
    Zeitwerk when using isolate_namespace.

--- lib/rtools/engine.rb
+++ lib/rtools/engine.rb
@@ -4,6 +4,13 @@ module Rtools
   class Engine < ::Rails::Engine
     isolate_namespace Rtools
 
+    # Explicitly add app/ to autoload paths
+    config.autoload_paths += %W[
+      #{root}/app/controllers
+      #{root}/app/services
+      #{root}/app/helpers
+    ]
+
     routes do
       resources :performance_profiles, only: %i[index show], format: false
     end
